06f1aeb14042c655e014ab49f34fdf61fe943973
Zaros <81772327+Sozialstunde@users.noreply.github.com>
2022-09-22 12:46:56 +0200

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -1,37 +1,27 @@
 import discord
-from discord.ext import commands
-from discord.ext.commands import Bot
-
 import asyncio
-import time
-import random
-import os
-import sys
-import json
-import datetime
-import traceback
-import logging
+
+
 
 TOKEN = 'MTAyMDY2NzMwNTIyODc2NzM1Mw.GWVcQe.vMImOqr_xwsFI0ik10EspSWjhIfu1dtIempq2k'
 
 einladung = 'test'
 
+Client = discord.Client()
+
 hilfe = ''
 
-bot = commands.Bot(command_prefix='!') 
-
-@bot.event
+@Client.event
 async def on_ready():
     print('Logged in as')
-    print(bot.user.name)
-    print(bot.user.id)
+    print(Client.user.name)
+    print(Client.user.id)
     print('------')
 
 async def on_message(message):
     if message.content.startswith('!discord'):
-        await bot.send_message(message.channel, einladung)
+        await Client.send_message(message.channel, einladung)
     if message.content.startswith('!help'):
-        await bot.send_message(message.channel, hilfe)
-    
+        await Client.send_message(message.channel, hilfe)
 
-bot.run(TOKEN, bot=True, reconnect=True)
+Client.run(TOKEN)
